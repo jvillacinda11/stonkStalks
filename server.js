be387b70f58b22ade9c589bbd67bf77f542d4d23
jvilla <josevillacinda96@gmail.com>
2021-03-22 11:57:46 -0700

    first push

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const { join } = require('path')
+const passport = require('passport')
+
+const app = express()
+
+app.use(express.static(join(__dirname, 'public')))
+app.use(express.urlencoded({ extended: true}))
+app.use(express.json)
+
+app.use(passport.initialize())
+app.use(passport.session())
+
+require('./db').sync()
+.then(() => app.listen(process.env.PORT || 3000))
+.catch(err => console.log(err))
