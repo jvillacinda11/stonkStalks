b8899c4171d4edc6941f95037fc21e375ac4474b
Shweta Sharma <dostind@gmail.com>
2021-03-24 10:32:56 -0700

    pushing the code

---
+++ routes/viewRoutes.js
@@ -0,0 +1,30 @@
+const router = require('express').Router()
+const { User } = require('../models')
+const passport = require('passport')
+const jwt = require('jsonwebtoken')
+
+
+
+
+
+router.get('/login', (req, res) => {
+  res.sendFile(join(__dirname, '..', 'public', 'login.html'))
+})
+
+router.get('/register', (req, res) => {
+  res.sendFile(join(__dirname, '..', 'public', 'register.html'))
+})
+
+// router.get('/loading', (req, res) => {
+//   res.sendFile(join(__dirname, '..', 'public', 'loading.html'))
+// })
+
+router.get('/*', (req, res) => {
+  res.sendFile(join(__dirname, '..', 'public', 'index.html'))})
+
+
+
+
+
+
+module.exports = router
--- server.js
+++ server.js
@@ -1,21 +1,34 @@
-//require('dotenv').config()
+require('dotenv').config()
 
 const express = require('express')
 // import sequelize connection
 const { join } = require('path')     
 const sequelize = require('./config/connection.js') 
 
-require('./models')  
+require('./models') 
+const passport = require('passport')
+const { User } = require('./models')  
 
 const app = express()
+
+app.use(express.static(join(__dirname, 'public')))
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+app.use(passport.initialize())
+app.use(passport.session())
+
+passport.use(User.createStrategy())
+passport.serializeUser(User.serializeUser())
+passport.deserializeUser(User.deserializeUser())
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-//app.use(require('./routes'))
+app.use(require('./routes'))
 
 // sync sequelize models to the database, then turn on the server
 require('./config/connection.js')
-  .sync()
+  .sync()                         //{ force: true }
   .then(() => app.listen(process.env.PORT || 3000))
   .catch(err => console.log(err))
