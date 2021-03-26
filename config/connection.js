514b909c4cab2819e50675a345360d1f65ccead8
Nick Palmer <ylnickpalmer@gmail.com>
2021-03-26 11:24:23 -0700

    completed merge

--- config/connection.js
+++ config/connection.js
@@ -2,6 +2,6 @@ require('dotenv').config()
 
 const { Sequelize } = require('sequelize')
 
-const sequelize = new Sequelize(process.env.JAWSDB_URL ? process.env.JAWSDB_URL : process.env.LOCAL_URL)
+const sequelize = new Sequelize(process.env.JAWSDB_URL ? process.env.JAWSDB_URL : process.env.LOCALDB_URL)
 
 module.exports = sequelize
